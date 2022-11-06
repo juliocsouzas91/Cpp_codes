a5a7f634d41ff199ab085f9e317f896d7a78a34f
Julio Souza <Juliocsouzas91@gmail.com>
2022-11-06 19:48:48 -0300

    Add string example

---
+++ Simple_Codes/Chapter4/Readme.txt
@@ -0,0 +1,16 @@
+Chapter goal:
+• How to create and use arrays
+• How to create and use C-style string
+• How to create and use string-class strings
+• How to use the getline() and get() methods for reading strings
+• How to mix string and numeric input
+• How to create and use structures
+• How to create and use unions
+• How to create and use enumerations
+• How to create and use pointers
+• How to manage dynamic memory with new and delete
+• How to create dynamic arrays
+• How to create dynamic structures
+• Automatic, static, and dynamic storage
+
+Programs:
--- Simple_Codes/Chapter4/array_exp.cpp
+++ Simple_Codes/Chapter4/array_exp.cpp
@@ -9,13 +9,37 @@ Date 06/11/2022*/
 #include<iostream>
 int main(){
     using namespace std;
+    /*-----------------Arrays------------------*/
     int array_variable[3];
     array_variable[0] = 7;
     array_variable[1] = 8;
     array_variable[2] = 9;
     cout << "Primeiro valor: " << array_variable[0] << ". Segundo valor: "<< array_variable[1] << ". Terceiro valor: "<< array_variable[2]<<"."<< endl;
+    
     //Another type of declaration
     int array_variable2[3] = {1, 2, 3};
     cout << "Primeiro valor: " << array_variable2[0] << ". Segundo valor: "<< array_variable2[1] << ". Terceiro valor: "<< array_variable2[2]<<"."<< endl;
+    /*Considerations:
+    We can partially initialize the array
+    It is possible to omit the [number] when declaring, in this way the compiler will alocate the memory
+    */
+   
+    /*
+    -----------------Srings----------------------
+    Srings are a series of characters stored in an array where the last character is the null value \0
+    This null character is crucial because there is implemented functions to handle strings*/
+    char bird[] = "Quero-Quero" ; // é considerado uma string
+    //Store a string
+    char name[10],surname[10];
+    cout<< "Enter your first name" <<endl;
+    cin >> name;
+    cout << "Hello "<< name << endl;
+    //Store a string with getline()
+    cout<< "Enter your surname" <<endl;
+    cin.get();
+    cin.getline(surname, 10);
+    cout << "Hello "<<name<<" "<<surname << endl;
+
+
     return 0;
 }
